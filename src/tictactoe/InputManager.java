25abb7aa56c2c66e2874e9a013ff00e2cea74641
Adrià Batlle <sudobat@gmail.com>
2024-02-08 18:52:35 +0100

    InputManager done by Brenda

--- src/tictactoe/InputManager.java
+++ src/tictactoe/InputManager.java
@@ -5,13 +5,24 @@ package tictactoe;
  */
 public class InputManager {
     public boolean validate(Board board, String input) {
-        return true;
+        int position = parse(input);
+        if (board.getState()[position] == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
      * Le va a entrar un String "1" - "9" y va a devolver un entero 1 - 9.
      */
     public int parse(String input) {
-        return 1;
+        try {
+            int parsedInput = Integer.parseInt(input);
+            return parsedInput;
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+        }
+        return 0;
     }
 }
