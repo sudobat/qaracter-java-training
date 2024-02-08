2d7d702f3248e229d1d1a8948799ac414c75ff09
javier.nakama <javier.nakama@qaracter.com>
2024-02-08 14:22:25 -0300

    BoardPrinter completed

--- src/tictactoe/BoardPrinter.java
+++ src/tictactoe/BoardPrinter.java
@@ -5,6 +5,21 @@ package tictactoe;
  */
 public class BoardPrinter {
     public void print(Board board) {
-
+        int [] entryData = {1,2,0,0,1,1,2,2,0};
+        char [] entryDataToChar = new char[9];
+        for(int i=0; i < 9 ;i++){
+            switch (entryData[i]){
+                case 0 -> entryDataToChar[i]= ' ';
+                case 1 -> entryDataToChar[i]= 'O';
+                case 2 -> entryDataToChar[i]= 'X';
+            }
+        }
+        System.out.println("───┬───┬───");
+        System.out.println(" " + entryDataToChar[0] + " │ " + entryDataToChar[1] + " │ " + entryDataToChar[2]);
+        System.out.println("───┼───┼───");
+        System.out.println(" " + entryDataToChar[3] + " │ " + entryDataToChar[4] + " │ " + entryDataToChar[5]);
+        System.out.println("───┼───┼───");
+        System.out.println(" " + entryDataToChar[6] + " │ " + entryDataToChar[7] + " │ " + entryDataToChar[8]);
+        System.out.println("───┴───┴───");
     }
 }
