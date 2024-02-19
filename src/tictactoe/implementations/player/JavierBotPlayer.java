2b7414c2d66dd97e5dc1cb28305b84a2d01bb225
javier.nakama <javier.nakama@qaracter.com>
2024-02-19 11:54:41 -0300

    JavierBotPlayer beta Commit

--- src/tictactoe/implementations/player/JavierBotPlayer.java
+++ src/tictactoe/implementations/player/JavierBotPlayer.java
@@ -4,30 +4,73 @@ import tictactoe.Player;
 import tictactoe.ReadOnlyBoard;
 
 public class JavierBotPlayer implements Player {
-    int myTurn;
-
-    public JavierBotPlayer(int myTurn) {
-        if (myTurn != 1 && myTurn != 2) {
-            this.myTurn = 1;
-        }
-
-        this.myTurn = myTurn;
-    }
 
     @Override
-    public String makeMove(ReadOnlyBoard board) {
+    public String makeMove(ReadOnlyBoard board, int myTurn) {
 
         int[] state = board.getState();
-        int movesCount = 0;
-        for (int i = 0; i < 9; i++) {
-            if (state[i] == 1 || state[i] == 2){
-                movesCount++;
+        int winningPosition;
+        winningPosition = findWinningMove(state);
+        if(winningPosition== -1){
+            for (int position = 0; position < 9; position++) {
+                if (state[position] == 0) {
+                    return String.valueOf(position + 1);
+                }
             }
         }
-        if(movesCount<2){
-            
+        else
+            return String.valueOf(winningPosition+1);
+        return "";
+    }
+
+    public int findWinningMove(int[] state) {
+        for (int i = 0; i < 9; i++) {
+            if (state[i] == 0) {
+                state[i] = this.myTurn;
+                if (checkWin(this.myTurn, state)) {
+                    state[i] = 0;
+                    return i;
+                }
+                state[i] = 0;
+            }
         }
 
-        return "";
+        int opponent;
+        if (this.myTurn == 1)
+            opponent = 2;
+        else
+            opponent = 1;
+        for (int i = 0; i < 9; i++) {
+            if (state[i] == 0) {
+                state[i] = opponent;
+                if (checkWin(opponent, state)) {
+                    state[i] = 0;
+                    return i;
+                }
+                state[i] = 0;
+            }
+        }
+        return -1;
+    }
+
+    private boolean checkWin(int player, int[] state) {
+        for (int i = 0; i < 3; i++) {
+            if (state[i * 3] == player && state[i * 3 + 1] == player && state[i * 3 + 2] == player) {
+                return true;
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (state[i] == player && state[i + 3] == player && state[i + 6] == player) {
+                return true;
+            }
+        }
+
+        if ((state[0] == player && state[4] == player && state[8] == player) ||
+                (state[2] == player && state[4] == player && state[6] == player)) {
+            return true;
+        }
+
+        return false;
     }
 }
