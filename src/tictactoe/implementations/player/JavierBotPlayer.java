7da92028592f4de91a6e81ccd281bf3ea2111c05
Adrià Batlle <sudobat@gmail.com>
2024-02-20 16:39:07 +0100

    Changes by javier.nakama

--- src/tictactoe/implementations/player/JavierBotPlayer.java
+++ src/tictactoe/implementations/player/JavierBotPlayer.java
@@ -9,48 +9,48 @@ public class JavierBotPlayer implements Player {
     public String makeMove(ReadOnlyBoard board, int myTurn) {
 
         int[] state = board.getState();
-        int winningPosition;
-        winningPosition = findWinningMove(state, myTurn);
-        if (winningPosition == -1) {
+//        int winningPosition;
+//        winningPosition = findWinningMove(state, myTurn);
+//        if (winningPosition == -1) {
             for (int position = 0; position < 9; position++) {
                 if (state[position] == 0) {
                     return String.valueOf(position + 1);
                 }
             }
-        } else
-            return String.valueOf(winningPosition + 1);
+//        } else
+//            return String.valueOf(winningPosition + 1);
         return "";
     }
 
-    public int findWinningMove(int[] state, int myTurn) {
-        for (int i = 0; i < 9; i++) {
-            if (state[i] == 0) {
-                state[i] = myTurn;
-                if (checkWin(myTurn, state)) {
-                    state[i] = 0;
-                    return i;
-                }
-                state[i] = 0;
-            }
-        }
-
-        int opponent;
-        if (myTurn == 1)
-            opponent = 2;
-        else
-            opponent = 1;
-        for (int i = 0; i < 9; i++) {
-            if (state[i] == 0) {
-                state[i] = opponent;
-                if (checkWin(opponent, state)) {
-                    state[i] = 0;
-                    return i;
-                }
-                state[i] = 0;
-            }
-        }
-        return -1;
-    }
+//    private int findWinningMove(int[] state, int myTurn) {
+//        for (int i = 0; i < 9; i++) {
+//            if (state[i] == 0) {
+//                state[i] = myTurn;
+//                if (checkWin(myTurn, state)) {
+//                    state[i] = 0;
+//                    return i;
+//                }
+//                state[i] = 0;
+//            }
+//        }
+//
+//        int opponent;
+//        if (myTurn == 1)
+//            opponent = 2;
+//        else
+//            opponent = 1;
+//        for (int i = 0; i < 9; i++) {
+//            if (state[i] == 0) {
+//                state[i] = opponent;
+//                if (checkWin(opponent, state)) {
+//                    state[i] = 0;
+//                    return i;
+//                }
+//                state[i] = 0;
+//            }
+//        }
+//        return -1;
+//    }
 
     private int checkWin(int player, int[] state) {
         for (int i = 0; i < 3; i++) {
@@ -64,15 +64,49 @@ public class JavierBotPlayer implements Player {
 
         for (int i = 0; i < 3; i++) {
             if (state[i] == player && state[i + 3] == player && state[i + 6] == player) {
-                return true;
+                if (state[i * 3]==player)
+                    return 10;
+                else
+                    return -10;
             }
         }
 
-        if ((state[0] == player && state[4] == player && state[8] == player) ||
-                (state[2] == player && state[4] == player && state[6] == player)) {
-            return true;
+//        if ((state[0] == player && state[4] == player && state[8] == player) ||
+//                (state[2] == player && state[4] == player && state[6] == player)) {
+//            if (state[i * 3]==player)
+//                return 10;
+//            else
+//                return -10;
+//        }
+
+        return 0;
+    }
+
+    private int scorePlay(int myTurn, int[] state) {
+        int opponent = (myTurn == 1) ? 2 : 1;
+        int bestMove = -1;
+        int bestScore = (myTurn == 1) ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        for (int i = 0; i < 9; i++) {
+            if (state[i] == 0) {
+                state[i] = myTurn;
+                int score = scorePlay(opponent, state);
+                state[i] = 0;
+
+                if (myTurn == 1) {
+                    if (score > bestScore) {
+                        bestScore = score;
+                        bestMove = i;
+                    }
+                } else {
+                    if (score < bestScore) {
+                        bestScore = score;
+                        bestMove = i;
+                    }
+                }
+            }
         }
 
-        return false;
+        return 0;
     }
 }
